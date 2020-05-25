85cf7556ead8f4e01d6558880325bb61749ae655
Nikita Kot <kotn01@outlook.cz>
2020-05-25 21:53:33 +0200

    generate types

--- src/graphql.schema.generated.ts
+++ src/graphql.schema.generated.ts
@@ -1,3 +1,4 @@
+
 /** ------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
  * -------------------------------------------------------
@@ -6,52 +7,50 @@
 /* tslint:disable */
 /* eslint-disable */
 export class LoginInput {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 export class PostInput {
-  title: string;
-  body?: string;
+    title: string;
+    body?: string;
 }
 
 export class SignUpInput {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 export class AuthPayload {
-  id: string;
-  email: string;
+    id: string;
+    email: string;
 }
 
 export abstract class IMutation {
-  abstract signup(
-    signUpInput?: SignUpInput,
-  ): AuthPayload | Promise<AuthPayload>;
+    abstract signup(signUpInput?: SignUpInput): AuthPayload | Promise<AuthPayload>;
 
-  abstract login(loginInput?: LoginInput): AuthPayload | Promise<AuthPayload>;
+    abstract login(loginInput?: LoginInput): AuthPayload | Promise<AuthPayload>;
 
-  abstract createPost(postInput?: PostInput): Post | Promise<Post>;
+    abstract createPost(postInput?: PostInput): Post | Promise<Post>;
 }
 
 export class Post {
-  id: string;
-  title: string;
-  body?: string;
-  author: User;
+    id: string;
+    title: string;
+    body?: string;
+    author: User;
 }
 
 export abstract class IQuery {
-  abstract post(id: string): Post | Promise<Post>;
+    abstract post(id: string): Post | Promise<Post>;
 
-  abstract posts(): Post[] | Promise<Post[]>;
+    abstract posts(): Post[] | Promise<Post[]>;
 }
 
 export class User {
-  id: string;
-  email: string;
-  post: Post[];
-  createdAt: string;
-  updatedAt: string;
+    id: string;
+    email: string;
+    post: Post[];
+    createdAt: string;
+    updatedAt: string;
 }
